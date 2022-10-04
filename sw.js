c4b1ffee9ccab68c3974f0a035418d50157a62c6
jlvFlores <jluisvazquez.jv@gmail.com>
2022-10-03 19:50:52 -0500

    48. Service Worker: Activate

--- sw.js
+++ sw.js
@@ -2,5 +2,9 @@
 // Ciclo de vida del SW
 
 self.addEventListener('install', event => {
-    console.log('SW: Instalando SW!!!');
+    console.log('SW: Instalando SW');
+});
+
+self.addEventListener('install', event => {
+    console.log('SW2: Activo y listo para controlar la app');
 });
