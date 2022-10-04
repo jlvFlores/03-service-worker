1256f14f6a12678ae88f3bc61817909028c77cdf
jlvFlores <jluisvazquez.jv@gmail.com>
2022-10-03 19:59:26 -0500

    49. event.waitUntil();

--- sw.js
+++ sw.js
@@ -3,8 +3,19 @@
 
 self.addEventListener('install', event => {
     console.log('SW: Instalando SW');
+
+    const Instalacion = new Promise( (resolve, reject) => {
+        setTimeout(() => {
+            console.log('SW: Instalaciones terminadas');
+            self.skipWaiting();
+            resolve();
+        }, 1000);
+    });
+
+    event.waitUntil( Instalacion );
+
 });
 
-self.addEventListener('install', event => {
+self.addEventListener('activate', event => {
     console.log('SW2: Activo y listo para controlar la app');
 });
